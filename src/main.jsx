5809ba6687db5efcc88b08b49dd6100f9cc9a5bc
Harish-cheenu <harish.cheenu@yahoo.com>
2024-08-06 23:17:11 +0530

    config update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, UserButton, useUser } from '@clerk/clerk-react';
 import App from './App';
 import './index.css';
--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
-export default defineConfig({
-  build: {
-    target: 'esnext'
-  },
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     federation({
       name: 'game-platform',
       remotes: {
-        colorMatchMaster: 'https://clickcolor.netlify.app/assets/colormatchmaster.js'
+        colorMatchMaster: (mode === 'dev' ?  "http://localhost:5001/dist/assets/colormatchmaster.js" : 'https://colormatchmaster.netlify.app/assets/colormatchmaster.js')
       },
       shared: ['react', 'react-dom']
     })
-  ]
-})
+  ],
+  build: {
+    target: "esnext",
+    cssCodeSplit: false,
+  }
+}))
