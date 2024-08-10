343bf5bb4fce83105dd8a0a8c36175360ee879dc
Harish-cheenu <harish.cheenu@yahoo.com>
2024-08-10 18:39:34 +0530

    improved local dev setup config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
+const COLOR_MATCH_MASTER_LOCAL =  "http://localhost:5001/dist/assets/colormatchmaster.js";
+const COLOR_MATCH_MASTER_LIVE =  "https://colormatchmaster.netlify.app/assets/colormatchmaster.js";
+
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     federation({
       name: 'game-platform',
       remotes: {
-        colorMatchMaster: (mode === 'dev' ?  "http://localhost:5001/dist/assets/colormatchmaster.js" : 'https://colormatchmaster.netlify.app/assets/colormatchmaster.js')
+        colorMatchMaster: (mode === 'dev' ? COLOR_MATCH_MASTER_LOCAL : COLOR_MATCH_MASTER_LIVE)
       },
       shared: ['react', 'react-dom']
     })
